39f26a554fdcf6c87ca0709db94af9ebcd3b2be2
Harshit Gupta <guptaharshit@microsoft.com>
2026-03-08 07:18:14 -0700

    Exit master.sh if ngrok tunnel setup fails
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- docker/master.sh
+++ docker/master.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e
+
 echo "Running ngrok daemon"
 ./ngrok-daemon.sh
+
+echo "ngrok status: $?"
 
 echo "Launching the app"
 ./app-runner.sh
--- docker/ngrok-daemon.sh
+++ docker/ngrok-daemon.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+set -e
+
 ngrok config add-authtoken ${NGROK_AUTH_TOKEN}
 
 ngrok http 8000 >/ngrok.log 2>&1 &
+ngrokPid=$!
 
 sleep 2
+
+if kill -0 "$ngrokPid" 2>/dev/null; then
+  echo "Process $ngrokPid is running. Safe to continue"
+else
+  echo "Process $ngrokPid is not running. Exiting."
+  exit 1
+fi
 
 ngrokPublicUrl=$(curl http://127.0.0.1:4040/api/tunnels | jq -r ".tunnels[0].public_url")
 
